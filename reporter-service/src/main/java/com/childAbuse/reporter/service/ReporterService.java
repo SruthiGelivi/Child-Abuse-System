4db4b16a5d686db518701d1ef988162201527183
sruthi <sruthigelivi@gmail.com>
2020-05-26 01:32:41 -0400

    feature-1: added few more docs and changes to pom

--- reporter-service/src/main/java/com/childAbuse/reporter/service/ReporterService.java
+++ reporter-service/src/main/java/com/childAbuse/reporter/service/ReporterService.java
@@ -1,14 +1,15 @@
 package com.childAbuse.reporter.service;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class ReporterService {
 
-    @RequestMapping("/home")
+    @RequestMapping("/")
     String home() {
         return "Hello World!";
     }
